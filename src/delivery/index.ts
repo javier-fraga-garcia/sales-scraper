4771e7d97cf5e8d017e09b167e53bb901bdccfb7
Javier Fraga García <javierfrga@gmail.com>
2026-04-25 20:02:45 +0200

    implemetar DeliveryService

--- src/delivery/index.ts
+++ src/delivery/index.ts
@@ -3,12 +3,32 @@ import type { Delivery } from "@/config/schema";
 class DeliveryService {
   constructor(private deliveryConfig: Delivery) {}
 
-  send(msg: string): void {
-    throw new Error("Método no implementado");
+  async send(msg: string): Promise<boolean> {
+    if (!this.deliveryConfig.enabled) return false;
+    switch (this.deliveryConfig.type) {
+      case "webhook":
+        return await this.sendWebhook(this.deliveryConfig, msg);
+      default:
+        throw new Error(
+          `Tipo de envío ${this.deliveryConfig.type} no implementado`,
+        );
+    }
   }
 
-  private sendWebhook(msg: string): void {
-    throw new Error("Método no implementado");
+  private async sendWebhook(
+    config: Extract<Delivery, { type: "webhook" }>,
+    msg: string,
+  ): Promise<boolean> {
+    const res = await fetch(config.url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: msg,
+      }),
+    });
+    return res.ok;
   }
 }
 
